b012d7c51befc09112ddadeb83a693e78dac3692
shenshijie <2460824642@qq.com>
2021-06-19 12:32:21 +0800

    seat最新

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,181 +1,233 @@
 <template>
-    <div id="app">
-        <button @click="Sort" style="position: relative; z-index: 999; width: 150px; height: 30px">
-            选择区域
-        </button>
-        <button
-            @click="insertPeople"
-            style="position: relative; width: 100px; height: 30px; z-index: 998"
-        >
-            导入人员数据
-        </button>
-        <!-- 	  <select v-model="index">
+  <div id="app">
+    <div style="display:flex;">
+      <button
+        @click="Sort"
+        style="position: relative; z-index: 999; width: 150px; height: 30px"
+      >选择区域</button>
+      <button
+        @click="changeVisibility"
+        v-show="isShown"
+        style="position: relative; z-index: 999; width: 150px; height: 30px"
+      >显示</button>
+      <button
+        @click="changeVisibility"
+        v-show="!isShown"
+        style="position: relative; z-index: 999; width: 150px; height: 30px"
+      >隐藏</button>
+      <button
+        @click="insertPeople"
+        style="position: relative; width: 100px; height: 30px; z-index: 998"
+      >导入人员数据</button>
+          <div
+      id="reset"
+      draggable="true"
+      @dragstart="handleDragStart($event)"
+      @dragover.prevent="handleDragOver($event)"
+      @dropstop="handleDrop($event)"
+    >
+      占位
+    </div>
+      <p style="position:relative;">{{sums}}</p>
+    </div>
+
+    <!-- 	  <select v-model="index">
 		  <option value="1">1</option>
 		  <option value="3">3</option>
 		  <option value="4">4</option>
 		  <option value="5">5</option>
-	  </select> -->
-        <vue-draggable-resizable
-            :w="980"
-            :h="490"
-            :x="370"
-            :y="10"
-            :min-width="50"
-            :min-height="50"
-            :parent="true"
-            :grid="[10, 10]"
-            @resizestop="onResizstop"
-            @dragstop="onDragstop"
-            class-name="dragging1"
-            @dragging="onDrag"
-            @resizing="onResize"
-        >
-            <p></p>
-        </vue-draggable-resizable>
-        <Hello
-            v-for="index of 8"
-            class="dragbox"
-            :options="option"
-            :selects="sec"
-            @aa="tty"
-            :txtlist="txtList"
-            :key="index"
-            :persons='persons'
-        ></Hello>
-    </div>
+    </select>-->
+    <vue-draggable-resizable
+      :w="980"
+      :h="490"
+      :x="370"
+      :y="10"
+            v-show="isShown"
+      :min-width="50"
+      :min-height="50"
+      :parent="true"
+      :grid="[10, 10]"
+      @resizestop="onResizstop"
+      @dragstop="onDragstop"
+      class-name="dragging1"
+      @dragging="onDrag"
+      @resizing="onResize"
+    >
+      <p></p>
+    </vue-draggable-resizable>
+    <Hello
+      v-for="index of 8"
+      class="dragbox"
+      :options="option"
+      :selects="sec"
+      @aa="tty"
+      :txtlist="txtList"
+      :key="index"
+      :persons="persons"
+      @showSum="showSums"
+    ></Hello>
+  </div>
 </template>
 
 <script>
 import Hello from "./com/kuang.vue";
 import children from "./com/children.vue";
 export default {
-    data() {
-        return {
-            width: 980,
-            height: 490,
-            x: 0,
-            y: 0,
-            xlist: [],
-            sec: null,
-            index: 1,
-            option: {},
-            persons: [],
-            txtList: {
-                x1: 370,
-                y1: 10,
-                x2: 980,
-                y2: 490,
-            },
-        };
-    },
-    components: {
-        Hello,
-        children,
-    },
+  data() {
+    return {
+      width: 980,
+      height: 490,
+      x: 0,
+      y: 0,
+      xlist: [],
+      sec: null,
+      index: 1,
+      option: {},
+      sums: 0,
+      persons: [],
+      txtList: {
+        x1: 370,
+        y1: 10,
+        x2: 980,
+        y2: 490
+      },
+      isShown: true
+    };
+  },
+  components: {
+    Hello,
+    children
+  },
 
-    mounted: function() {
-        
+  mounted: function() {},
+  methods: {
+    onDragstop(x, y, width, height) {
+      this.txtList.x1 = x + 40;
+      this.txtList.y1 = y + 20;
+      this.txtList.y2 = y + this.height + 30;
+      this.txtList.x2 = x + this.width;
     },
-    methods: {
-        onDragstop(x, y, width, height) {
-            this.txtList.x1 = x+20;
-            this.txtList.y1 = y+20;
-            this.txtList.y2 = y+this.height+40;
-            this.txtList.x2 = x+this.width;
-        },
-        onResizstop(x, y, width, height) {
-            this.txtList.x1 = x+20;
-            this.txtList.y1 = y+20;
-            this.txtList.x2 = x+this.width+20;
-            this.txtList.y2 = y+this.height+20;
-            console.log(this.txtList)
-        },
-        onResize: function(x, y, width, height) {
-            this.x = x;
-            this.y = y;
-            this.width = width;
-            this.height = height;
-        },
-        onDrag: function(x, y) {
-            this.x = x;
-            this.y = y;
-        },
-        tty(a) {},
-        setChildren() {
-            this.test = {
-                name: "lisi",
-                age: 5,
-            };
-            console.log(111, this.test);
-        },
-        Sort: function() {
-            
-            const app = this;
-            let data = {
-                confSeatList: this.GLOBAL.baseURL,
-                sortType: 1,
-            };
-            console.log(this.GLOBAL.baseURL)
-            this.$axios
-                .getSort(data)
-                .then((res) => {
-                    app.option = res.data.data;
-                })
-                .catch((err) => {});
-                
-        },
-        insertPeople() {
-            
-            let data = {
-                blocks: [
-                    {
-                        id: 1,
-                        num: 1,
-                    },
-                ],
-                confSeats: this.GLOBAL.baseURL,
-            };
-            this.$axios
-                .getPeople(data)
-                .then((res) => {
-                    this.persons = res.data.data.confSeats;
-                })
-                .catch((err) => {});
-        },
+    onResizstop(x, y, width, height) {
+      this.txtList.x1 = x + 20;
+      this.txtList.y1 = y + 20;
+      this.txtList.x2 = x + this.width + 20;
+      this.txtList.y2 = y + this.height + 20;
+      console.log(this.txtList);
     },
+    onResize: function(x, y, width, height) {
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+    },
+    onDrag: function(x, y) {
+      this.x = x;
+      this.y = y;
+    },
+    tty(a) {},
+    showSums(data) {
+      this.sums = data;
+    },
+    setChildren() {
+      this.test = {
+        name: "lisi",
+        age: 5
+      };
+      console.log(111, this.test);
+    },
+    Sort: function() {
+      const app = this;
+      let data = {
+        confSeatList: this.GLOBAL.baseURL,
+        sortType: 1
+      };
+      console.log(this.GLOBAL.baseURL);
+      this.$axios
+        .getSort(data)
+        .then(res => {
+          app.option = res.data.data;
+        })
+        .catch(err => {});
+    },
+    insertPeople() {
+      let data = {
+        blocks: [
+          {
+            id: 1,
+            num: 1
+          }
+        ],
+        confSeats: this.GLOBAL.baseURL
+      };
+      this.$axios
+        .getPeople(data)
+        .then(res => {
+          this.persons = res.data.data.confSeats;
+        })
+        .catch(err => {});
+    },
+    changeVisibility(){
+        this.isShown = !this.isShown
+    },
+        // 拖拽开始
+    handleDragStart() {
+      //   this.dragging = item;
+      console.log('开始拖拽')
+    },
+    //首先把div变成可以放置的元素，即重写dragenter/dragover
+    // handleDragOver(e) {
+    //   e.preventDefault();
+    //   console.log(e);
+    //   console.log('handleDragOver');
+    //   // e.dataTransfer.dropEffect = "move"; // e.dataTransfer.dropEffect="move";//在dragenter中针对放置目标来设置!
+    // },
+    // // 鼠标释放
+    // handleDrop(e){
+    //     console.log(e);
+    //     console.log('释放');
+    //   e.currentTarget.innerText = ''
+    // }
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 #selectbc {
-    background-color: rebeccapurple;
+  background-color: rebeccapurple;
 }
 
 .st {
-    width: 50px;
-    height: 50px;
-    border: 1px so;
+  width: 50px;
+  height: 50px;
+  border: 1px so;
 }
 .dragbox {
-    width: 100%;
-    margin: 20px;
+  width: 100%;
+  margin: 20px;
 }
 .dragbox,
 img {
-    -webkit-user-select: none;
-    -webkit-user-drag: none;
+  -webkit-user-select: none;
+  -webkit-user-drag: none;
 }
 .draging {
-    background-color: aquamarine;
+  background-color: aquamarine;
 }
 
 * {
-    cursor: default;
+  cursor: default;
 }
 .dragging1 {
-    position: absolute;
-    border: 1px solid #000;
-    color: #000;
+  position: absolute;
+  border: 1px solid #000;
+  color: #000;
+}
+#reset {
+  width: 100px;
+  height: 30px;
+  line-height: 30px;
+  text-align: center;
+  background-color: gray;
 }
 </style>
--- src/components/com/kuang.vue
+++ src/components/com/kuang.vue
@@ -1,233 +1,262 @@
 <template>
-    <div id="kz" style="display: flex">
-        <div v-for="index of 5" class="main" :key="index.id">
-            <div class="table"></div>
-            <div style="display: flex">
-                <div class="t1" v-for="index of 6" :key="index.id"></div>
-            </div>
-        </div>
+  <div id="kz" style="display: flex">
+    <div v-for="index of 5" class="main" :key="index.id" :gindex="index">
+      <div class="table"></div>
+      <div style="display: flex">
+        <div
+          class="t1"
+          v-for="index of 6"
+          :key="index.id"
+          @drop="handleDrop($event)"
+          @dragover="handleDragOver($event)"
+        ></div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Hello",
-    props: {
-        txtlist:{
-            type:Object,
-            default:function () {
-            return {
-                x1: 370,
-                y1: 10,
-                x2: 980,
-                y2: 490,
-            }
-            }
-        },
-        persons:Array,
-        options:Object,
-
-    },
-    data() {
+  name: "Hello",
+  props: {
+    txtlist: {
+      type: Object,
+      default: function() {
         return {
-            xinxi: [],
-            items: [],
-            seat: [],
-            sort: [],
-            sum: 0,
-            lastSeat:[]
+          x1: 370,
+          y1: 10,
+          x2: 980,
+          y2: 490
         };
+      }
     },
-    created() {
-        const app = this;
-        this.$axios
-            .getDistributeData()
-            .then((res) => {
-                app.xinxi = res.data.data.confSeats;
-            })
-            .catch(function(res) {
-                console.log(res.data);
-            });
+    persons: Array,
+    options: Object
+  },
+  data() {
+    return {
+      xinxi: [],
+      items: [],
+      seat: [],
+      sort: [],
+      sum: 0,
+      lastSeat: []
+    };
+  },
+  created() {
+    const app = this;
+    this.$axios
+      .getDistributeData()
+      .then(res => {
+        app.xinxi = res.data.data.confSeats;
+      })
+      .catch(function(res) {
+        console.log(res.data);
+      });
+  },
+  mounted: function() {
+    var rector = $("#selectionRect");
+
+    //拖选起点
+    var startX = 0;
+    var startY = 0;
+    //选择框核心代码
+
+    function resizeToPoint(x, y) {
+      var width = Math.abs(x - startX);
+      var height = Math.abs(y - startY);
+      rector.width(width);
+      rector.height(height);
+      //当鼠标x方向上为向左拖动 设置left位置
+      if (x < startX) {
+        rector.css({
+          left: startX - width + "px"
+        });
+      }
+      //当鼠标y方向上为向上拖动 设置top位置
+      if (y < startY) {
+        rector.css({
+          top: startY - height + "px"
+        });
+      }
+    }
+
+    /**
+     *核心相交算法
+     * @param rect1{x1,y1,x2,y2}
+     * @param rect2 {x1,y1,x2,y2}
+     */
+
+    /**
+     *获取元素的矩形的起始点坐标与其对角点坐标
+     * @param $el
+     * @return {x1,y1,x2,y2}
+     */
+
+    //框选处理 如果元素与选择框相交则设置样式
+  },
+  watch: {
+    options: {
+      handler: function(newVal, oldVal) {
+        this.seatSort();
+        // console.log(11111,newVal,oldVal)
+        // console.log(this.options)
+      },
+      deep: true
     },
-    mounted: function() {
-        var rector = $("#selectionRect");
-
-        //拖选起点
-        var startX = 0;
-        var startY = 0;
-        //选择框核心代码
-
-        function resizeToPoint(x, y) {
-            var width = Math.abs(x - startX);
-            var height = Math.abs(y - startY);
-            rector.width(width);
-            rector.height(height);
-            //当鼠标x方向上为向左拖动 设置left位置
-            if (x < startX) {
-                rector.css({
-                    left: startX - width + "px",
-                });
-            }
-            //当鼠标y方向上为向上拖动 设置top位置
-            if (y < startY) {
-                rector.css({
-                    top: startY - height + "px",
-                });
-            }
-        }
-
-        /**
-         *核心相交算法
-         * @param rect1{x1,y1,x2,y2}
-         * @param rect2 {x1,y1,x2,y2}
-         */
-        
-        /**
-         *获取元素的矩形的起始点坐标与其对角点坐标
-         * @param $el
-         * @return {x1,y1,x2,y2}
-         */
-        
-        
-        //框选处理 如果元素与选择框相交则设置样式
-        
-
-        
-        
+    persons: {
+      handler: function(newVal, oldVal) {
+        this.sortPerson();
+      },
+      deep: true
     },
-    watch: {
-        options: {
-            handler: function(newVal, oldVal) {
-                this.seatSort();
-                // console.log(11111,newVal,oldVal)
-                // console.log(this.options)
-            },
-            deep: true,
-        },
-        persons: {
-            handler: function(newVal, oldVal) {
-                
-                this.sortPerson();
-            },
-            deep: true,
-        },
-        txtlist:{
-            handler:function(newVal,oldVal){
-                this.handleRectSelection(this.txtlist)
-            },
-            deep:true,
-        }
-    },
-    methods: {
-        seatSort() {
-            var oT1 = document.querySelectorAll(".t1");
-            for (var i = 0; i < oT1.length; i++) {
-                for (var j = 0; j < this.lastSeat.length; j++) {
-                    if (i == this.lastSeat[j]) {
-                        var a = "";
-                        var r,w = 0;
-                        r = Math.ceil((i + 1) / 30);
-                        w = parseInt((i + 1) % 30);
-                        a = r + "-" + w;
-                        oT1[i].innerText = this.options[a];
-                        // oT1[i].className += "choose";
-                        this.GLOBAL.baseURL[j]["orderMark"] = parseInt(this.options[a]);
-                    }
-                }
-            }
-            
-        },
-        handleRectSelection(data) {
+    txtlist: {
+      handler: function(newVal, oldVal) {
+        this.handleRectSelection(this.txtlist);
+      },
+      deep: true
+    }
+  },
+  methods: {
+    seatSort() {
+      var oT1 = document.querySelectorAll(".t1");
+
+      for (var i = 0; i < oT1.length; i++) {
+        for (var j = 0; j < this.lastSeat.length; j++) {
+          if (i == this.lastSeat[j]) {
             var a = "";
-            const app = this;
-            
-            $(".t1").each(function(index) {
-                var rect = app.getRect($(this));
-                if (app.isCross(data, rect)) {
-                    app.xinxi[index]["blockId"] = 1;
-                    app.xinxi[index].merge=2
-                    console.log(app.xinxi[index])
-                    app.seat.push(index);
-                    app.items.push(app.xinxi[index]);
-                    $(this).addClass("selected");
-                } else {
-                    $(this).text('')
-                    $(this).removeClass("selected");
-                }
-            });
-            this.GLOBAL.baseURL = app.items;
-            app.lastSeat =app.seat;
-            app.seat=[]
-            app.items=[]
-        },
-        getRect($el) {
-            var x1 = $el.offset().left;
-            var y1 = $el.offset().top;
-            var x2 = x1 + $el.outerWidth();
-            var y2 = y1 + $el.outerHeight();
-            return { x1, x2, y1, y2 };
-        },
-        isCross(rect1, rect2) {
-            var xNotCross = true; //x方向上不重合
-            var yNotCross = true; //y方向上不重合
-            xNotCross = rect1.x1 > rect2.x2 || rect2.x1 > rect1.x2;
-            yNotCross = rect1.y1 > rect2.y2 || rect2.y1 > rect1.y2;
-            return !(xNotCross || yNotCross);
-        },
-        sortPerson(){
-            var oT1=document.querySelectorAll(".t1");
-			for(var i = 0;i<oT1.length;i++){
-				
-				for (var j=0;j<this.seat.length;j++){
-					if(i == this.seat[j]){
-                        
-                        console.log(this.persons[j]['userUnit'].des)
-						oT1[i].innerText=this.persons[j]['userUnit'].des
-						
-					}
-					
-				}
-			}
-		}
+            var r,
+              w = 0;
+            r = Math.ceil((i + 1) / 30);
+            w = parseInt((i + 1) % 30);
+            a = r + "-" + w;
+            if (this.options[a] != null) {
+              oT1[i].innerText = this.options[a];
+              // oT1[i].className += "choose";
+            } else {
+              oT1[i].innerText = "已占位";
+            }
+            this.GLOBAL.baseURL[j]["orderMark"] = parseInt(this.options[a]);
+          }
+        }
+      }
     },
+    handleRectSelection(data) {
+      var a = "";
+      const app = this;
+
+      $(".t1").each(function(index) {
+        var rect = app.getRect($(this));
+        if (app.isCross(data, rect)) {
+          var b=0;
+          app.xinxi[index]["blockId"] = 1;
+          b=$(this).text()
+          console.log(b)
+          if(b == ''){
+              app.xinxi[index].merge = 0
+          }else{
+              app.xinxi[index].merge = 2;
+          }
+          app.seat.push(index);
+          app.items.push(app.xinxi[index]);
+          $(this).addClass("selected");
+        } else {
+        //   $(this).text("");
+          $(this).removeClass("selected");
+        }
+      });
+      this.GLOBAL.baseURL = app.items;
+      app.sum = app.items.length;
+      app.$emit("showSum", app.sum);
+      app.lastSeat = app.seat;
+      app.seat = [];
+      app.items = [];
+    },
+    getRect($el) {
+      var x1 = $el.offset().left;
+      var y1 = $el.offset().top;
+      var x2 = x1 + $el.outerWidth();
+      var y2 = y1 + $el.outerHeight();
+      return { x1, x2, y1, y2 };
+    },
+    isCross(rect1, rect2) {
+      var xNotCross = true; //x方向上不重合
+      var yNotCross = true; //y方向上不重合
+      xNotCross = rect1.x1 > rect2.x2 || rect2.x1 > rect1.x2;
+      yNotCross = rect1.y1 > rect2.y2 || rect2.y1 > rect1.y2;
+      return !(xNotCross || yNotCross);
+    },
+    sortPerson() {
+      console.log(this.persons);
+      var oT1 = document.querySelectorAll(".t1");
+      for (var i = 0; i < oT1.length; i++) {
+        for (var j = 0; j < this.lastSeat.length; j++) {
+          if (i == this.lastSeat[j]) {
+            if (this.persons[j].merge == 0) {
+              oT1[i].innerText = this.persons[j]["userUnit"].des;
+            } else {
+              oT1[i].innerText = "已占位";
+            }
+          }
+        }
+      }
+    },
+    // 进入时
+    // handleDragEnter(e) {
+    //   console.log('拖拽进入')
+    //   console.log(e.currentTarget);
+    //   e.currentTarget.innerText = ''
+    // },
+    handleDrop(e) {
+      e.preventDefault();
+      e.currentTarget.innerText = "占位";
+    },
+    handleDragOver(e) {
+      e.preventDefault();
+    }
+    // 拖拽结束
+    // handleDragEnd(){
+    //     console.log('拖拽结束');
+    // }
+  }
 };
 </script>
 
 <style scoped>
 .t1 {
-    width: 40px;
-    height: 20px;
-    margin: 5px;
-    background-color: rgb(135, 145, 153);
-    text-align: center;
-    color: #fff;
+  width: 40px;
+  height: 20px;
+  margin: 5px;
+  background-color: rgb(135, 145, 153);
+  text-align: center;
+  color: #fff;
 }
 .selected {
-    width: 40px;
-    height: 20px;
-    margin: 5px;
-    text-align: center;
-    color: #fff;
-    background-color: red;
+  width: 40px;
+  height: 20px;
+  margin: 5px;
+  text-align: center;
+  color: #fff;
+  background-color: red;
 }
 .selects {
-    width: 40px;
-    height: 20px;
-    margin: 5px;
-    text-align: center;
-    color: #fff;
-    background-color: #ff0;
+  width: 40px;
+  height: 20px;
+  margin: 5px;
+  text-align: center;
+  color: #fff;
+  background-color: #ff0;
 }
 .choose {
-    background-color: #000088;
+  background-color: #000088;
 }
 .table {
-    width: 300px;
-    height: 30px;
-    background-color: rgb(191, 205, 214);
+  width: 300px;
+  height: 30px;
+  background-color: rgb(191, 205, 214);
 }
 .main {
-    margin-left: 30px;
-    margin-bottom: 20px;
+  margin-left: 30px;
+  margin-bottom: 20px;
 }
 </style>
